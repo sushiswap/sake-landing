8c01a1363b8f980f932d0d161aebc1f416c6ce74
omakasebar <omakasebar@gmail.com>
2021-05-17 02:36:02 -0500

    add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,31 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+import * as gtag from "../core/analytics";
 import "../styles/globals.css";
-import "@fontsource/vt323";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Head>
+        <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+import { GA_TRACKING_ID } from "../core/analytics";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,6 +12,17 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+          <meta name="application-name" content={process.env.APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content={process.env.APP_NAME} />
+
+          <meta name="description" content={process.env.NEXT_PUBLIC_APP_DESCRIPTION} />
+          <meta name="keywords" content={process.env.NEXT_PUBLIC_APP_KEYWORDS} />
+
           <link
             rel="preload"
             href="/fonts/BPdotsSquareBold-webfont.woff"
@@ -17,8 +30,58 @@ class MyDocument extends Document {
             type="font/woff"
             crossOrigin="anonymous"
           />
+
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+
+          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta name="msapplication-TileColor" content={"#000000"} />
+          <meta name="msapplication-tap-highlight" content="no" />
+
+          <meta name="theme-color" content={"#000000"} />
+
+          {/* <link rel="apple-touch-icon" href="/apple-icon.png"></link> */}
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link href="/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+          <link href="/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+
+          {/* TWITTER */}
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content={process.env.APP_URL} />
+          <meta name="twitter:title" content={process.env.APP_NAME} />
+          <meta name="twitter:description" content={process.env.APP_DESCRIPTION} />
+          <meta name="twitter:image" content="/android-chrome-192x192.png" />
+          <meta name="twitter:creator" content="@MatthewLilley" />
+
+          {/* FACEBOOK */}
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={process.env.APP_NAME} />
+          <meta property="og:description" content={process.env.APP_DESCRIPTION} />
+          <meta property="og:site_name" content={process.env.APP_NAME} />
+          <meta property="og:url" content={process.env.APP_URL} />
+          <meta property="og:image" content="/apple-touch-icon.png" />
+          <script type="text/javascript" src="https://app.intotheblock.com/widget.js" />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                });
+                `,
+            }}
+          />
         </Head>
-        <body>
+        <body className="no-js">
           <Main />
           <NextScript />
         </body>
